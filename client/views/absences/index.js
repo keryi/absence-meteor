f2b6264f6275ec7bd32500b05e40f95dd865e872
keryi <hti_32@yahoo.com>
2014-12-30 10:34:58 +0800

    Removed unused file: index.js for absence

--- client/views/absences/index.js
+++
@@ -1,8 +0,0 @@
-Template.absenceIndex.helpers({
-	absences: function() {
-		var currentYear = new Date().getFullYear();
-		var start = new Date(currentYear, 0, 1);
-		var end = new Date(currentYear + 1, 11, 31);
-		return Absences.find({ startDate: { $gte: start, $lte: end } });
-	}
-});
